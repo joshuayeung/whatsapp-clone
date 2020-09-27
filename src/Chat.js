fd24673557df6b5a438c5cd73e0d6c9a8d9c2b78
Joshua Yeung <joshua.chyeung@gmail.com>
2020-09-27 16:24:46 +0800

    ADD: random avatar & addNewChat

--- src/Chat.js
+++ src/Chat.js
@@ -2,18 +2,36 @@ import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons/";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import MicIcon from "@material-ui/icons/Mic";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Chat.css";
+import axios from "./axios";
 
 function Chat({ messages }) {
-  const sendMessage = (e) => {
+  const [seed, setSeed] = useState("");
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  const [input, setInput] = useState("");
+
+  const sendMessage = async (e) => {
     e.preventDefault();
+
+    await axios.post("/api/v1/messages/new", {
+      message: input,
+      name: "Demo App",
+      timestamp: "Just now!",
+      received: false,
+    });
+
+    setInput("");
   };
 
   return (
     <div className="chat">
       <div className="chat__header">
-        <Avatar />
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
 
         <div className="chat__headerInfo">
           <h3>Room name</h3>
@@ -54,7 +72,12 @@ function Chat({ messages }) {
           action="
         "
         >
-          <input placeholder="Type a message" type="text" />
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Type a message"
+            type="text"
+          />
           <button onClick={sendMessage} type="submit">
             Send a message
           </button>
